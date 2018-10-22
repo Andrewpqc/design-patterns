7620d3c7b324a0e73a8367d98bc94fb3f4efaaf1
Andrewpqc <m13636038496@163.com>
2018-10-22 08:55:24 +0800

    feat: update

--- iterator/src/Main.java
+++ iterator/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         bookShelf.append(new Book("A"));
         bookShelf.append(new Book("B"));
         bookShelf.append(new Book("C"));
+        bookShelf.append(new Book("D"));
         Iterator<Book> bookShelfIterator = bookShelf.iterator();
         while (bookShelfIterator.hasNext()) {
             System.out.println(bookShelfIterator.next().getBookName());
