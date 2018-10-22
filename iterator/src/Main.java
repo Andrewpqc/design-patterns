805a176c0f0c017f7f115884ca0a0fc960117a80
Andrewpqc <m13636038496@163.com>
2018-10-22 09:13:04 +0800

    .

--- iterator/src/Main.java
+++ iterator/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         BookShelf bookShelf = new BookShelf(10);
         bookShelf.append(new Book("A"));
         bookShelf.append(new Book("B"));
